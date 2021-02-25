abc3cc447013bdfd63041fcdc5dbd19942fdba09
TondeMoon <tanek8981@gmail.com>
2021-02-25 12:56:22 -0800

    added changes as pa-8

---
+++ frontend/src/actions/index.tsx
@@ -0,0 +1,20 @@
+import { Action } from 'redux';
+
+export enum CounterTypes {
+  INCREASE_COUNT = 'INCREASE_COUNT',
+  DECREASE_COUNT = 'DECREASE_COUNT',
+}
+
+export interface CounterState {
+  count: number;
+}
+
+export interface IncrementAction extends Action {
+  type: CounterTypes.INCREASE_COUNT;
+}
+
+export interface DecrementAction extends Action {
+  type: CounterTypes.DECREASE_COUNT;
+}
+
+export type CounterActions = IncrementAction | DecrementAction;
