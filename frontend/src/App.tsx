d9917c535036772841d3ebb8e8ebb3b93cc6f361
TondeMoon <tanek8981@gmail.com>
2021-02-26 02:30:22 -0800

    feat(PA-4): add integration front+back via webpack-bundle-tracker, eject react-scripts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import Counter from './components/counter/Counter';
@@ -6,6 +6,9 @@ import counterReducer from '../src/reducers/counterReducer';
 const store = createStore(counterReducer);
 
 const App: React.FC = () => {
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_DJANGO_SERVER}/api/v1/hello/`)
+  })
   return (
     <div className="container page">
       <h1>Welcome to Project-alpha!</h1>
