60ec1e4a3618856ea0faef4852d879f7476b2cea
Ivan Gerasin <ivan@gerasin.dev>
2021-02-28 20:34:02 +0700

    feat(PA-21): add eslint

--- frontend/src/actions/Counter.tsx
+++ frontend/src/actions/Counter.tsx
@@ -1,9 +1,9 @@
-import { CounterTypes } from './index';
+import {CounterActions, CounterTypes} from './index'
 
-export const increaseCount = () => ({
+export const increaseCount = (): CounterActions => ({
   type: CounterTypes.INCREASE_COUNT,
-});
+})
 
-export const decreaseCount = () => ({
+export const decreaseCount = (): CounterActions => ({
   type: CounterTypes.DECREASE_COUNT,
-});
+})
--- frontend/src/reducers/counterReducer.tsx
+++ frontend/src/reducers/counterReducer.tsx
@@ -1,20 +1,20 @@
-import { CounterTypes, CounterState, CounterActions } from '../actions';
-import { Action } from 'redux';
+import { CounterActions, CounterState, CounterTypes } from '../actions'
+import { Action } from 'redux'
 
-const initialState: CounterState | any = {
+const initialState: CounterState = {
   count: 0,
-};
+}
 
 export default function counterReducer(
   state: CounterState = initialState,
   action: Action | CounterActions
-) {
+): CounterState {
   switch (action.type) {
     case CounterTypes.INCREASE_COUNT:
-      return { ...state, count: state.count + 1 };
+      return { ...state, count: state.count + 1 }
     case CounterTypes.DECREASE_COUNT:
-      return { ...state, count: state.count - 1 };
+      return { ...state, count: state.count - 1 }
     default:
-      return state;
+      return state
   }
 }
