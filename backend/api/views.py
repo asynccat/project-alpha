38e90ea68e2c742a87c50f01811bb5a03a7efc69
jlllk <ehyf.py@gmail.com>
2021-03-01 20:20:19 +0300

    feat(PA-26): create sign up endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,21 @@
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .serializers import UserSerializer
+
+
+User = get_user_model()
 
 
 class HelloView(APIView):
     def get(self, request):
         return Response({'status': 'hello'})
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
