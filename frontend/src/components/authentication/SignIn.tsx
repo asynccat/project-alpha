8c2a867080aaae8424a724017f7be4df14275275
Nina Orlova <tanek8981@gmail.com>
2021-03-05 04:14:57 -0800

    feat/pa-30-add-redux-state-and-actions

--- frontend/src/components/authentication/SignIn.tsx
+++ frontend/src/components/authentication/SignIn.tsx
@@ -1,66 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-magic-numbers */
 /* eslint-disable react/jsx-no-bind */
 import React, {useState } from 'react'
 import {connect} from 'react-redux'
-import Avatar from '@material-ui/core/Avatar'
-import Button from '@material-ui/core/Button'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import TextField from '@material-ui/core/TextField'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Checkbox from '@material-ui/core/Checkbox'
-import Link from '@material-ui/core/Link'
-import Paper from '@material-ui/core/Paper'
-import Box from '@material-ui/core/Box'
-import Grid from '@material-ui/core/Grid'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
+import {Dispatch} from 'redux'
+
 import { fetchUser } from '../../actions/userActions'
+import { ActionsOfUser, UserDetailsOnLogin } from '../../actions'
+import {useStyles} from './SignInStyles'
 
-
-function Copyright() :React.ReactElement {
-  return (
-    <Typography align="center" color="textSecondary" variant="body2">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-      Project-Alpha
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  )
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}))
+import {CssBaseline, TextField} from '@material-ui/core'
+import {Button, Avatar, Typography, FormControlLabel, Checkbox, Link, Paper, Grid }  from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 
 export function SignInSide() :React.ReactElement {
 	const initialFormData = Object.freeze({
@@ -81,7 +32,7 @@ export function SignInSide() :React.ReactElement {
 		e.preventDefault()
 		console.log(formData)
     fetchUser(formData)
-
+  }
 
   const classes = useStyles()
 
@@ -157,20 +108,16 @@ export function SignInSide() :React.ReactElement {
                 </Link>
               </Grid>
             </Grid>
-            <Box mt={5}>
-              <Copyright />
-            </Box>
           </form>
         </div>
       </Grid>
     </Grid>
   )
 }
-}
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch:Dispatch<ActionsOfUser>) => {
   return {
-      fetchUser: (formData) => dispatch(fetchUser(formData))
+      fetchUser: (formData: UserDetailsOnLogin) => dispatch<any>(fetchUser(formData))
   }
 }
 
--- frontend/src/components/authentication/SignUp.tsx
+++ frontend/src/components/authentication/SignUp.tsx
@@ -1,67 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-magic-numbers */
 /* eslint-disable react/jsx-no-bind */
 import React, {useState } from 'react'
 import {connect} from 'react-redux'
+import {Dispatch} from 'redux'
+import {useHistory} from 'react-router-dom'
+
 import {signUserUp} from '../../actions/userActions'
-import Avatar from '@material-ui/core/Avatar'
-import Button from '@material-ui/core/Button'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import TextField from '@material-ui/core/TextField'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Checkbox from '@material-ui/core/Checkbox'
-import Link from '@material-ui/core/Link'
-import Paper from '@material-ui/core/Paper'
-import Box from '@material-ui/core/Box'
-import Grid from '@material-ui/core/Grid'
+import { fetchUserAction, UserDetailsOnRegister } from '../../actions'
+import {useStyles} from './SignUpStyles'
+
+import {CssBaseline, TextField} from '@material-ui/core'
+import {Button, Avatar, Typography, FormControlLabel, Checkbox, Link, Paper, Grid }  from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
 
-function Copyright(): React.ReactElement {
-  return (
-    <Typography align="center" color="textSecondary" variant="body2">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-       Project-Alpha
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  )
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}))
-
-export function SignUpSide() :React.ReactElement {
+function SignUpSide() :React.ReactElement {
+  const history = useHistory()
 	const initialFormData = Object.freeze({
 		email: '',
 		username: '',
@@ -80,7 +34,9 @@ export function SignUpSide() :React.ReactElement {
 	const handleSubmit = (e:React.SyntheticEvent) => {
 		e.preventDefault()
     signUserUp(formData)
-
+    history.push('/login')
+   
+  }
   const classes = useStyles()
 
   return (
@@ -170,20 +126,15 @@ export function SignUpSide() :React.ReactElement {
           </Grid>
         </form>
       </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
       </Grid>
     </Grid>
   )
 }
-}
 
-
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch:Dispatch<fetchUserAction>) => {
   return {
-      signUserUp: (formData) => dispatch(signUserUp(formData))
-  }
+      signUserUp: (formData: UserDetailsOnRegister) => dispatch<any>(signUserUp(formData))
+}
 }
 
 export default connect(null, mapDispatchToProps)(SignUpSide)
