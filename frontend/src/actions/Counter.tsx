b9c835c20f84850e6c79d359bcc6090e39244331
Ivan Gerasin <ivan@gerasin.dev>
2021-03-09 13:53:28 +0700

    refactoring(PA-30): remove counter code

--- frontend/src/actions/Counter.tsx
+++
@@ -1,9 +0,0 @@
-import {CounterActions, CounterTypes} from './authActions'
-
-export const increaseCount = (): CounterActions => ({
-  type: CounterTypes.INCREASE_COUNT,
-})
-
-export const decreaseCount = (): CounterActions => ({
-  type: CounterTypes.DECREASE_COUNT,
-})
