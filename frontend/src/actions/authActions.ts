06361aa44fbe7aad7298d75c70b52531069cf406
Nina Orlova <tanek8981@gmail.com>
2021-03-17 00:36:11 -0700

    bugfix(pa-31): correct thunk and 404

---
+++ frontend/src/actions/authActions.ts
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import {Dispatch} from 'redux'
+
+import {actionCreator} from '../redux-utils/actionCreator'
+import {Action} from '../types/action'
+import {AuthApiClient } from '../api/authRequest'
+import {IUser} from '../models/user'
+
+export enum AuthActionType {
+  LOG_OUT = 'auth/LOG_OUT',
+  SET_USER = 'auth/SET_USER'
+}
+
+export interface IUserDetails {
+  email: string
+  password: string
+}
+
+export interface IUserAuthApiResponse {
+  token: string
+  user: IUser
+}
+
+export type fetchUserAction = Action<AuthActionType.SET_USER, IUser>
+export type logoutUserAction = Action<AuthActionType.LOG_OUT>
+
+export type AuthActions = fetchUserAction | logoutUserAction
+
+export const setUserAction = actionCreator<AuthActionType.SET_USER, IUser>(AuthActionType.SET_USER)
+
+// TODO: localStorage.clear()
+export const logoutAction = actionCreator<AuthActionType.LOG_OUT>(AuthActionType.LOG_OUT)
+
+export const signUserUp = (dispatch:Dispatch<fetchUserAction>, payload: IUserDetails): Promise<void> => 
+// @ts-ignore
+async dispatch => {
+  const authApiClient = new AuthApiClient()
+
+  try {
+    const result: IUserAuthApiResponse = await authApiClient.register(payload)
+    console.log(result)
+    localStorage.setItem('token', result.token)
+    dispatch(setUserAction(result.user))
+  } catch (e) {
+    console.log('Error:', e)
+  }
+}
+
+// @ts-ignore
+export const login = (dispatch:Dispatch<fetchUserAction>, payload: IUserDetails): Promise<void> => async dispatch => {
+  const authApiClient = new AuthApiClient()
+
+  try {
+    const result = await authApiClient.login(payload)
+    localStorage.setItem('token', result.token)
+    dispatch(setUserAction(result.user))
+  } catch (e) {
+    console.log('Error:', e)
+  }
+}
+
+// TODO: action - token refresh
