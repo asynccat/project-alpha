636daab2ea7f0b9bc26e3b8df8e9840744b09aae
Stanislav Shiltsin <ehyf.py@gmail.com>
2021-03-21 15:27:51 +0300

    feat(PA-44): add profile endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,13 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-from .serializers import UserSerializer
+from .serializers import (
+    UserSerializer,
+    UserProfileSerializer
+)
+
 
 User = get_user_model()
 
@@ -14,3 +19,21 @@ class UserCreateAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny, )
+
+
+class UserProfileAPIView(generics.RetrieveAPIView):
+    """
+    Retrieve user profile data.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+    lookup_field = 'nickname'
+    permission_classes = (AllowAny,)
+
+    def get(self, request, nickname):
+        user_data = {
+            'nickname': nickname,
+            'email': f'{nickname}@gmail.com',
+            'avatar': '/path/to/avatar.png',
+        }
+        return Response(user_data)
