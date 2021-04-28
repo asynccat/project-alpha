2f96fcf87b7d44a4e697526a6198f1a8ecd6497f
Stas Shiltsin <ehyf.py@gmail.com>
2021-04-28 13:06:40 +0300

    feat(PA-50): change nickname timeout message

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 from rest_framework.permissions import BasePermission
+
+from .utils import get_word_ending
 
 
 class IsOwner(BasePermission):
@@ -15,12 +18,17 @@ class IsOwner(BasePermission):
 
 
 class NicknameUpdateAllowed(BasePermission):
+    message = _('You cannot change your nickname.')
+
     def has_object_permission(self, request, view, obj):
         nickname_updated = obj.usersettings.nickname_updated
 
         if nickname_updated:
             today = timezone.now()
             last_nickname_update = today - nickname_updated
+            if last_nickname_update.days < settings.NICKNAME_UPDATE_TIMEOUT_DAYS:
+                nickname_timeout = settings.NICKNAME_UPDATE_TIMEOUT_DAYS - last_nickname_update.days
+                self.message = _(f'You can change your nickname in {get_word_ending(nickname_timeout)}')
             return last_nickname_update.days >= settings.NICKNAME_UPDATE_TIMEOUT_DAYS
 
         return True
