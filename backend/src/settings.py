7bfd211bf10f9e06029bfc7294a6bd73c49c37c6
Stas Shiltsin <ehyf.py@gmail.com>
2021-04-28 17:02:32 +0300

    feat(PA-39): made the error messages the same

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -126,6 +126,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'EXCEPTION_HANDLER': 'backend.api.utils.custom_exception_handler',
 }
 
 AUTHENTICATION_BACKENDS = (
