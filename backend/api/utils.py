53eb52b9843ae6056cefa6d36b01e337d710f6d6
Stas Shiltsin <ehyf.py@gmail.com>
2021-04-28 13:06:40 +0300

    feat(PA-50): change nickname timeout message

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -8,3 +8,9 @@ def get_tokens_for_user(user):
         'refresh': str(refresh),
         'access': str(refresh.access_token),
     }
+
+
+def get_word_ending(days_number):
+    if days_number == 1:
+        return '1 day'
+    return f'{days_number} days'
