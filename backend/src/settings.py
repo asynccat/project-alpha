c97f4f271dd03dcdf620eae06d38d71cfb2e215c
Ivan Gerasin <ivan@gerasin.dev>
2021-05-08 08:57:13 +0700

    test(PA-61): add tests for change password view

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -126,6 +126,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
     'EXCEPTION_HANDLER': 'api.utils.custom_exception_handler',
 }
 
