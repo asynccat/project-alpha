c76896e0cb1fff55cf4e44763a6a079c10dcdb47
Alexander1 <aleks.pishchulin@gmail.com>
2021-05-29 19:21:01 +0300

    feat PA-28: feat refactor: change file password_validator

--- backend/project_alpha/web/utils/password_validator.py
+++ backend/project_alpha/web/utils/password_validator.py
@@ -8,10 +8,10 @@ class MinimumLengthValidator:
     def __init__(self, min_length=8):
         self.min_length = min_length
 
-    def validate(self, password):
+    def validate(self, password, user=None):
         if len(password) < self.min_length:
             raise ValidationError(
-                _("This password must contain at least %(min_length)d characters."),
+                _("The password must contain at least %(min_length)d characters."),
                 code='password_too_short',
                 params={'min_length': self.min_length},
             )
@@ -30,7 +30,7 @@ class NumberValidator:
     def __init__(self, min_digits=0):
         self.min_digits = min_digits
 
-    def validate(self, password):
+    def validate(self, password, user=None):
         if not len(re.findall(r'\d', password)) >= self.min_digits:
             raise ValidationError(
                 _("The password must contain at least %(min_digits)d digit(s), 0-9."),
@@ -48,7 +48,7 @@ class UppercaseValidator:
     """
     Checks for uppercase letters in a password
     """
-    def validate(self, password):
+    def validate(self, password, user=None):
         if not re.findall(r'[A-Z]', password):
             raise ValidationError(
                 _("The password must contain at least 1 uppercase letter, A-Z."),
@@ -65,7 +65,7 @@ class LowercaseValidator:
     """
     Checks for lowercase letters in a password
     """
-    def validate(self, password):
+    def validate(self, password, user=None):
         if not re.findall(r'[a-z]', password):
             raise ValidationError(
                 _("The password must contain at least 1 lowercase letter, a-z."),
