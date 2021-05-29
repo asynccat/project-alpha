d4d3c0f4fea0c181c4307e8d2c769cb3ddab478e
Alexander1 <aleks.pishchulin@gmail.com>
2021-05-29 18:55:58 +0300

    feat PA-28: feat refactor: change file password_validator

--- backend/project_alpha/web/utils/password_validator.py
+++ backend/project_alpha/web/utils/password_validator.py
@@ -8,7 +8,7 @@ class MinimumLengthValidator:
     def __init__(self, min_length=8):
         self.min_length = min_length
 
-    def validate(self, password, user=None):
+    def validate(self, password):
         if len(password) < self.min_length:
             raise ValidationError(
                 _("This password must contain at least %(min_length)d characters."),
@@ -23,14 +23,14 @@ class MinimumLengthValidator:
         )
 
 
-class NumberValidator(object):
+class NumberValidator:
     """
     Checks for digits in a password
     """
     def __init__(self, min_digits=0):
         self.min_digits = min_digits
 
-    def validate(self, password, user=None):
+    def validate(self, password):
         if not len(re.findall(r'\d', password)) >= self.min_digits:
             raise ValidationError(
                 _("The password must contain at least %(min_digits)d digit(s), 0-9."),
@@ -44,11 +44,11 @@ class NumberValidator(object):
         )
 
 
-class UppercaseValidator(object):
+class UppercaseValidator:
     """
     Checks for uppercase letters in a password
     """
-    def validate(self, password, user=None):
+    def validate(self, password):
         if not re.findall(r'[A-Z]', password):
             raise ValidationError(
                 _("The password must contain at least 1 uppercase letter, A-Z."),
@@ -61,11 +61,11 @@ class UppercaseValidator(object):
         )
 
 
-class LowercaseValidator(object):
+class LowercaseValidator:
     """
     Checks for lowercase letters in a password
     """
-    def validate(self, password, user=None):
+    def validate(self, password):
         if not re.findall(r'[a-z]', password):
             raise ValidationError(
                 _("The password must contain at least 1 lowercase letter, a-z."),
