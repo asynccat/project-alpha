5c6adfa7c2b2a0201185e3895c2a413251d581bf
Belsnikel <afterouy@gmail.com>
2021-05-29 11:19:57 +0300

    added test for password

--- frontend/src/reducers/passwordStrength.test.js
+++ frontend/src/reducers/passwordStrength.test.js
@@ -8,7 +8,28 @@ describe('checkPasswordStrength', () => {
 });
 
 describe('checkPasswordStrength', () => {
-    function doTest()
-})
+    it('does password contains numbers and symbols', function checkPassword(checkPasswordStrength(password, is_digits, is_specials)) {
+
+            let is_digits = false; 
+            let is_specials = false; 
+         
+            let digits = "0123456789"; 
+            let specials = "!@#$%^&*()_-+=\|/.,:;[]{}";
+             
+             for (let i = 0; i < password.length; i++) {
+        
+               
+               if (!is_digits && (digits.includes(password[i])) != -1) {
+               	is_digits = true;
+               }
+               else if (!is_specials && (specials.includes(password[i])) != -1) {
+               	is_specials = true;
+               }
+        
+             }
+        
+             return (is_digits, is_specials);
+    }
+});
 
 
