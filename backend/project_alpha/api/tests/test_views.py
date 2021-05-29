d706315d0a55f69395c9707edd3a2800f7661e19
Ivan Gerasin <ivan@gerasin.dev>
2021-05-29 18:19:53 +0700

    refactor(PA-70): reorganize file structure of backend

---
+++ backend/project_alpha/api/tests/test_views.py
@@ -0,0 +1,81 @@
+import json
+from django.conf import settings
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from project_alpha.web.models import User
+
+
+class ChangePasswordViewTestCase(APITestCase):
+
+    def setUp(self):
+        self.username = 'test@example.com'
+        self.pwd = '1234'
+        self.user = User.objects.create_user(self.username, self.pwd)
+
+    def request(self, data):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(reverse('change_password'), data=data, format='json')
+        return response.status_code, json.loads(response.content)
+
+    def test_happy_path(self):
+        status_code, _ = self.request({
+            'old_password': self.pwd,
+            'new_password': '4321',
+            'confirm_password': '4321',
+        })
+
+        self.assertEqual(status_code, 200)
+        self.assertTrue(self.user.check_password('4321'))
+
+    def test_require_missing_fields(self):
+        status_code, content = self.request({
+            'old_password': self.pwd,
+            'new_password': '4321',
+        })
+
+        self.assertEqual(status_code, 400)
+        self.assertEqual(content['error'], 'Required fields are missing or empty.')
+
+        status_code, content = self.request({
+            'new_password': '4321',
+            'confirm_password': '4321',
+        })
+
+        self.assertEqual(status_code, 400)
+        self.assertEqual(content['error'], 'Required fields are missing or empty.')
+
+        status_code, content = self.request({
+            'new_password': '4321',
+            'old_password': None,
+            'confirm_password': '4321',
+        })
+
+        self.assertEqual(status_code, 400)
+        self.assertEqual(content['error'], 'Required fields are missing or empty.')
+
+    def test_change_old_password_incorrect(self):
+        status_code, content = self.request({
+            'old_password': 'wrong password',
+            'new_password': '4321',
+            'confirm_password': '4321',
+        })
+        self.assertEqual(status_code, 400)
+        self.assertEqual(content['error'], 'Password is incorrect')
+
+    def test_new_and_confirm_does_not_match(self):
+        status_code, content = self.request({
+            'old_password': self.pwd,
+            'new_password': '4321',
+            'confirm_password': '1111',
+        })
+
+        self.assertEqual(status_code, 400)
+        self.assertEqual(content['error'], 'New passwords does not match')
+
+    def test_require_authorization(self):
+        response = self.client.post(reverse('change_password'), data={
+            'old_password': self.pwd,
+            'new_password': '4321',
+            'confirm_password': '4321',
+        }, format='json')
+        self.assertEqual(response.status_code, 401)
---
+++ backend/project_alpha/api/urls.py
@@ -0,0 +1,24 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from .views import (
+    UserCreateAPIView,
+    UserProfileAPIView,
+    UserPreferencesAPIView,
+    UpdateNicknameAPIView,
+    ChangeUserPassword,
+)
+
+v1_router = DefaultRouter()
+
+urlpatterns = [
+    path('v1/sign-up/', UserCreateAPIView.as_view(), name='sign_up'),
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/user/<str:nickname>/nickname/update/', UpdateNicknameAPIView.as_view(), name='nickname_update'),
+    path('v1/profile/<str:nickname>/', UserProfileAPIView.as_view(), name='user_profile'),
+    path('v1/preferences/', UserPreferencesAPIView.as_view(), name='preferences'),
+    path('v1/change_password/', ChangeUserPassword.as_view(), name='change_password'),
+    path('v1/', include(v1_router.urls)),
+]
