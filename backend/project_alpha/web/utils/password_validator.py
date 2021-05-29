2d071bb07dde57338c167dc3efe7b142f8cd859b
Alexander1 <aleks.pishchulin@gmail.com>
2021-05-29 18:27:53 +0300

    feat PA-28: feat refactor: change file password_validator and replace it to utils, change settings auth_password_validators

--- backend/project_alpha/web/utils/password_validator.py
+++ backend/project_alpha/web/utils/password_validator.py
@@ -4,6 +4,25 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
 
 
+class MinimumLengthValidator:
+    def __init__(self, min_length=8):
+        self.min_length = min_length
+
+    def validate(self, password, user=None):
+        if len(password) < self.min_length:
+            raise ValidationError(
+                _("This password must contain at least %(min_length)d characters."),
+                code='password_too_short',
+                params={'min_length': self.min_length},
+            )
+
+    def get_help_text(self):
+        return _(
+            "Your password must contain at least %(self.min_length)d characters."
+            % {'self.min_length': self.min_length}
+        )
+
+
 class NumberValidator(object):
     """
     Checks for digits in a password
