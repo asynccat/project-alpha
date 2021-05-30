139b51677f10f21e7abd5a903f1afe370391c107
Belsnikel <afterouy@gmail.com>
2021-05-30 12:37:49 +0300

    added second  passwordTest

--- frontend/src/utils/passwordStrength.ts
+++ frontend/src/utils/passwordStrength.ts
@@ -1,8 +1,6 @@
-// let password = function getPassword(str) {
-// 	return str;
-// }
 
-function checkPasswordStrength(password:string) { 
+
+function checkPasswordStrength(password:String) { 
 	let result = (password.length > 6) ? true : false;
   return result;
  }
@@ -11,32 +9,3 @@ function checkPasswordStrength(password:string) {
 
 
  
-
-
-//  function checkPasswordTest(password) {
-    
-//     let is_digits = false; 
-//     let is_specials = false; 
- 
-//     let digits = "0123456789"; 
-//     let specials = "!@#$%^&*()_-+=\|/.,:;[]{}";
-     
-//      for (let i = 0; i < password.length; i++) {
-
-       
-//        if (!is_digits && (digits.indexOf(password[i])) != -1) {
-//        	is_digits = true;
-//        }
-//        else if (!is_specials && (specials.indexOf(password[i])) != -1) {
-//        	is_specials = true;
-//        }
-
-//      }
-
-//      return (is_digits, is_specials);
-     
-//  }
- 
-//  console.log(checkPasswordStrength(password('1!')));
- 
-//  console.log(checkPasswordTest(password('1!')));
