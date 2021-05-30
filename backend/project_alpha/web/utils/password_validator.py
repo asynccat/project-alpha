37a8c6881610beaa53a754e2bae1587e34cdc8f0
Alexander1 <aleks.pishchulin@gmail.com>
2021-05-30 11:48:37 +0300

    other(PA-28): add to password_validator pylint: disable=unused-argument

--- backend/project_alpha/web/utils/password_validator.py
+++ backend/project_alpha/web/utils/password_validator.py
@@ -9,6 +9,7 @@ class MinimumLengthValidator:
         self.min_length = min_length
 
     def validate(self, password, user=None):
+        # pylint: disable=unused-argument
         if len(password) < self.min_length:
             raise ValidationError(
                 _("The password must contain at least %(min_length)d characters."),
@@ -31,6 +32,7 @@ class NumberValidator:
         self.min_digits = min_digits
 
     def validate(self, password, user=None):
+        # pylint: disable=unused-argument
         if not len(re.findall(r'\d', password)) >= self.min_digits:
             raise ValidationError(
                 _("The password must contain at least %(min_digits)d digit(s), 0-9."),
@@ -49,6 +51,7 @@ class UppercaseValidator:
     Checks for uppercase letters in a password
     """
     def validate(self, password, user=None):
+        # pylint: disable=unused-argument
         if not re.findall(r'[A-Z]', password):
             raise ValidationError(
                 _("The password must contain at least 1 uppercase letter, A-Z."),
@@ -66,6 +69,7 @@ class LowercaseValidator:
     Checks for lowercase letters in a password
     """
     def validate(self, password, user=None):
+        # pylint: disable=unused-argument
         if not re.findall(r'[a-z]', password):
             raise ValidationError(
                 _("The password must contain at least 1 lowercase letter, a-z."),
