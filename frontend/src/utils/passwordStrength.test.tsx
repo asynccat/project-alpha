978732e47af29681073dc45ceb4735fd30909874
Belsnikel <afterouy@gmail.com>
2021-06-06 18:08:21 +0300

    deleted old comments

--- frontend/src/utils/passwordStrength.test.tsx
+++ frontend/src/utils/passwordStrength.test.tsx
@@ -4,64 +4,64 @@ import passwordStrength, { checkPasswordStrength,
 
 describe('Password strength', () => {
   test('returns true if password length is greater than 6', () => {
-    const longPassword = '1234567' //подготавливаешь тестовые данные
-    const result = passwordStrength(longPassword) // вызываешь свою функцию с тестовыми данными
-    expect(result).toBe(true) //проверка результата утверждением
+    const longPassword = '1234567' 
+    const result = passwordStrength(longPassword) 
+    expect(result).toBe(true) 
   })
 
   test('returns false if password length is less than 6', () => {
-    const shortPassword = 'ldfk' //подготавливаешь тестовые данные
-    const result = passwordStrength(shortPassword) // вызываешь свою функцию с тестовыми данными
-    expect(result).toBe(false) //проверка результата утверждением
+    const shortPassword = 'ldfk' 
+    const result = passwordStrength(shortPassword) 
+    expect(result).toBe(false) 
   })
 
 
   test('returns false if password is not a string', () => {
-    const wrongTypePassword = 12134; //подготавливаешь тестовые данные
-    const result = passwordStrength(wrongTypePassword) // вызываешь свою функцию с тестовыми данными
-    expect(result).toBe(false) //проверка результата утверждением
+    const wrongTypePassword = '12134';
+    const result = passwordStrength(wrongTypePassword)
+    expect(result).toBe(false) 
   })
 
 
 /* testing hasPasswordDigits*/
   
   test('returns true if password has digits', () => {
-    const numbersInPassword = '12345'; //подготавливаешь тестовые данные
-    const result = hasPasswordDigits(numbersInPassword) // вызываешь свою функцию с тестовыми данными
-    expect(result).toBe(true) //проверка результата утверждением
+    const numbersInPassword = '12345'; 
+    const result = hasPasswordDigits(numbersInPassword) 
+    expect(result).toBe(true) 
   })
   test('returns false if password hasn`t digits', () => {
-    const lettersInPassword = 'daslkgl'; //подготавливаешь тестовые данные
-    const result = hasPasswordDigits(lettersInPassword) // вызываешь свою функцию с тестовыми данными
-    expect(result).toBe(false) //проверка результата утверждением
+    const lettersInPassword = 'daslkgl'; 
+    const result = hasPasswordDigits(lettersInPassword) 
+    expect(result).toBe(false) 
   })
 
   /* testing hasPasswordLowerCaseLetters*/
 
   test('returns true if password has lowerCase letters', () => {
-    const LowerCaseInPassword = 'abcdef'; //подготавливаешь тестовые данные
-    const result = hasPasswordLowerCaseLetters(LowerCaseInPassword) // вызываешь свою функцию с тестовыми данными
-    expect(result).toBe(true) //проверка результата утверждением
+    const LowerCaseInPassword = 'abcdef'; 
+    const result = hasPasswordLowerCaseLetters(LowerCaseInPassword) 
+    expect(result).toBe(true) 
   })
 
   test('returns false if password hasn`t lowerCase letters', () => {
-    const UpperCaseInPassword = 'ASGJASJNBTJ'; //подготавливаешь тестовые данные
-    const result = hasPasswordLowerCaseLetters(UpperCaseInPassword) // вызываешь свою функцию с тестовыми данными
-    expect(result).toBe(false) //проверка результата утверждением
+    const UpperCaseInPassword = 'ASGJASJNBTJ'; 
+    const result = hasPasswordLowerCaseLetters(UpperCaseInPassword) 
+    expect(result).toBe(false)
   })
 
   /* testing hasPasswordLowerCaseLetters*/
 
   test('returns true if password has UpperCase letters', () => {
-    const UpperCaseInPassword = 'SDAGHDHA!@!$'; //подготавливаешь тестовые данные
-    const result = hasPasswordUpperCaseLetters(UpperCaseInPassword) // вызываешь свою функцию с тестовыми данными
-    expect(result).toBe(true) //проверка результата утверждением
+    const UpperCaseInPassword = 'SDAGHDHA!@!$'; 
+    const result = hasPasswordUpperCaseLetters(UpperCaseInPassword) 
+    expect(result).toBe(true) 
   })
 
   test('returns false if password hasn`t UpperCase letters', () => {
-    const lowerCaseInPassword= '1sdgh12423'; //подготавливаешь тестовые данные
-    const result = hasPasswordUpperCaseLetters(lowerCaseInPassword) // вызываешь свою функцию с тестовыми данными
-    expect(result).toBe(false) //проверка результата утверждением
+    const lowerCaseInPassword= '1sdgh12423'; 
+    const result = hasPasswordUpperCaseLetters(lowerCaseInPassword) 
+    expect(result).toBe(false) 
   })
 })
   
