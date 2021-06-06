569d990d0c8ce35f7a10fa5d4162942f003bbd8b
Belsnikel <afterouy@gmail.com>
2021-06-06 17:21:29 +0300

    adding functions for checking that password has lowerUpperCase letter & numbers, also unit tests

--- frontend/src/utils/passwordStrength.test.tsx
+++ frontend/src/utils/passwordStrength.test.tsx
@@ -1,6 +1,12 @@
 
-import passwordStrength from './passwordStrength'
+import passwordStrength, { checkPasswordStrength,
+  hasPasswordDigits, hasPasswordLowerCaseLetters, hasPasswordUpperCaseLetters} from './passwordStrength';
 
+<<<<<<< HEAD
+=======
+/* testing checkPasswordStrength*/
+
+>>>>>>> 7045beaf... adding functions for checking that password has lowerUpperCase letter & numbers, also unit tests
 describe('Password strength', () => {
   test('returns true if password length is greater than 6', () => {
     const longPassword = '1234567' //подготавливаешь тестовые данные
@@ -14,12 +20,55 @@ describe('Password strength', () => {
     expect(result).toBe(false) //проверка результата утверждением
   })
 
+<<<<<<< HEAD
   test('returns false if password is not a string', () => {
     const wrongTypePassword = 12134; //подготавливаешь тестовые данные
     const result = passwordStrength(wrongTypePassword) // вызываешь свою функцию с тестовыми данными
     expect(result).toBe(false) //проверка результата утверждением
   })
   //второй тест допиши сам :)
+=======
+/* testing hasPasswordDigits*/
+  
+  test('returns true if password has digits', () => {
+    const numbersInPassword = '12345'; //подготавливаешь тестовые данные
+    const result = hasPasswordDigits(numbersInPassword) // вызываешь свою функцию с тестовыми данными
+    expect(result).toBe(true) //проверка результата утверждением
+  })
+  test('returns false if password hasn`t digits', () => {
+    const lettersInPassword = 'daslkgl'; //подготавливаешь тестовые данные
+    const result = hasPasswordDigits(lettersInPassword) // вызываешь свою функцию с тестовыми данными
+    expect(result).toBe(false) //проверка результата утверждением
+  })
+
+  /* testing hasPasswordLowerCaseLetters*/
+
+  test('returns true if password has lowerCase letters', () => {
+    const LowerCaseInPassword = 'abcdef'; //подготавливаешь тестовые данные
+    const result = hasPasswordLowerCaseLetters(LowerCaseInPassword) // вызываешь свою функцию с тестовыми данными
+    expect(result).toBe(true) //проверка результата утверждением
+  })
+
+  test('returns false if password hasn`t lowerCase letters', () => {
+    const UpperCaseInPassword = 'ASGJASJNBTJ'; //подготавливаешь тестовые данные
+    const result = hasPasswordLowerCaseLetters(UpperCaseInPassword) // вызываешь свою функцию с тестовыми данными
+    expect(result).toBe(false) //проверка результата утверждением
+  })
+
+  /* testing hasPasswordLowerCaseLetters*/
+
+  test('returns true if password has UpperCase letters', () => {
+    const UpperCaseInPassword = 'SDAGHDHA!@!$'; //подготавливаешь тестовые данные
+    const result = hasPasswordUpperCaseLetters(UpperCaseInPassword) // вызываешь свою функцию с тестовыми данными
+    expect(result).toBe(true) //проверка результата утверждением
+  })
+
+  test('returns false if password hasn`t UpperCase letters', () => {
+    const lowerCaseInPassword= '1sdgh12423'; //подготавливаешь тестовые данные
+    const result = hasPasswordUpperCaseLetters(lowerCaseInPassword) // вызываешь свою функцию с тестовыми данными
+    expect(result).toBe(false) //проверка результата утверждением
+  })
+>>>>>>> 7045beaf... adding functions for checking that password has lowerUpperCase letter & numbers, also unit tests
 })
   
 
--- frontend/src/utils/passwordStrength.ts
+++ frontend/src/utils/passwordStrength.ts
@@ -1,10 +1,62 @@
+<<<<<<< HEAD
 
 
 function checkPasswordStrength(password:String) { 
   return password.length > 6
  }
+=======
+const passwordLength = 6;
+
+function checkPasswordStrength(password:String) { 
+  return password.length > passwordLength;
+ };
+>>>>>>> 7045beaf... adding functions for checking that password has lowerUpperCase letter & numbers, also unit tests
  
- export default checkPasswordStrength
+
+ function hasPasswordDigits(password:String) {
+    
+  let is_digits = false; 
+
+<<<<<<< HEAD
+ 
+=======
+  let digits = "0123456789"; 
+  
+   
+   for (let i = 0; i < password.length; i++) {
+     if (!is_digits && (digits.includes(password[i])) != false) {
+       is_digits = true;
+     }
+   }
+
+   return (is_digits);
+   
+};
+
+function hasPasswordLowerCaseLetters(password:String) {
+  return password.toUpperCase() != password;
+}
+
+function hasPasswordUpperCaseLetters(password:String) {
+  return password.toLowerCase() != password;
+}
 
 
- 
+export default checkPasswordStrength;
+
+ export { 
+   checkPasswordStrength,
+   hasPasswordDigits,
+   hasPasswordLowerCaseLetters,
+   hasPasswordUpperCaseLetters,
+ }
+
+
+// let is_specials = false; 
+
+// let specials = "!@#$%^&*()_-+=\|/.,:;[]{}";
+
+// else if (!is_specials && (specials.indexOf(password[i])) != -1) {
+//   is_specials = true;
+// }
+>>>>>>> 7045beaf... adding functions for checking that password has lowerUpperCase letter & numbers, also unit tests
