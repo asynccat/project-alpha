f9844647da435f43fa5403057d6db4e6d8ff553f
Belsnikel <afterouy@gmail.com>
2021-07-02 16:23:26 +0300

    created userPasswordvalidator which combined all the validators

--- frontend/src/utils/passwordStrength.ts
+++ frontend/src/utils/passwordStrength.ts
@@ -6,30 +6,62 @@ import React from "react";
     return passwordLength.test(password);
   };
   
-
-
-  const validatePassword = (password:string) => {
-
-    const validateContainsDigit = () => {
-      const digits = /0123456789/; 
-      return digits.test(password);
+  
+  
+   
+  const userPasswordValidator = (password:string) => {
+  
+    const invalidPassword = {
+      length: 'Length should be at least 6 letters',
+      hasdigits: 'Password should has digits',
+      hasymbols: 'password should has at least 1 symbols'
+    };
+    
+    const passwordFeatures = {  
+      digits: '(\\d.*)',
+      letters: '([a-zA-Z].*)',
+      symbols: '([`~\\!@#\\$%\\^\\&\\*\\(\\)\\-_\\=\\+\\[\\\{\\}\\]\\\\\|;:\\\'",<.>\\/\\?€£¥₹§±].*)'
     };
   
-    const validateContainsLowerCaseLetters= () => {
-        const regexp = /[a-z]/;
-        return (regexp.test(password)); 
-    };
+      const validateMinimalLength = () => {
+        const minimalPasswordLength = 6;
+        if(password.length === minimalPasswordLength  || password.length > minimalPasswordLength) {
+        return true;
+        } else {
+        return invalidPassword.length;
+        }
+      }
   
-    const validateContainsUpperCaseLetters = () => {
-        const regexp = /[A-Z]/;
-        return(regexp.test(password));
-    };
+      const validateContainsDigit = () => {
+        const digits = /0123456789/; 
+        return digits.test(password);
+      };
+    
+      const validateContainsLowerCaseLetters = () => {
+          const regexp = /[a-z]/;
+          return (regexp.test(password)); 
+      };
+    
+      const validateContainsUpperCaseLetters = () => {
+          const regexp = /[A-Z]/;
+          return(regexp.test(password));
+      };
+      
   
-    return(validateContainsDigit(), validateContainsLowerCaseLetters(), validateContainsUpperCaseLetters());
+      return (
+      validateMinimalLength(),
+      validateContainsDigit(),
+      validateContainsLowerCaseLetters(),
+      validateContainsUpperCaseLetters()
+      )
   }
+  
+  
+  
+  
 
 
  export { 
   validateMinimalLength,
-  validatePassword
+  userPasswordValidator
  }
