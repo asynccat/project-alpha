5695ff0cbf5390036b68aeaf3ac7dc76bbce8bd1
Belsnikel <afterouy@gmail.com>
2021-07-08 14:48:36 +0300

    added myCustomPasswordValidator

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,5 +2,6 @@ export const config = {
   baseUrl: process.env.REACT_APP_DJANGO_SERVER,
   apiV1: '/api/v1',
   MIN_PASSWORD_LEN: 6,
+  MIN_ADMIN_PASSWORD_LEN: 10,
   MINIMAL_VALIDATION_NUMBER: 3,
 }
--- frontend/src/utils/passwordStrength.ts
+++ frontend/src/utils/passwordStrength.ts
@@ -6,8 +6,7 @@ import {config} from "../config"
     const passLength = password.split("").length;
     return passLength > config.MIN_PASSWORD_LEN; 
   };
-  
-
+   
     const validateContainsDigit = (password:string) => {
       const digits = /[0-9]/; 
       return digits.test(password);
@@ -32,7 +31,37 @@ import {config} from "../config"
       const regexp = /[\s]/;
       return(regexp.test(password));
     };
+     
+    const createLenghtValidator = validateMinimalLength;
+  
+    const createValidator = (arr, minimalValidators:number) => {
+      let result = [];
 
+      for(let func of arr) {
+        result.push(func());
+      }
+      if(result.filter(Boolean).length == minimalValidators) {
+        return true;
+      }
+      
+    }
+ 
+
+    const myCustomPasswordValidator = createValidator([
+      createLenghtValidator(config.MIN_PASSWORD_LEN),
+      validateContainsDigit,
+      validateContainsLowerCaseLetters,
+      validateContainsUpperCaseLetters
+      ], config.MINIMAL_VALIDATION_NUMBER); 
+      
+    
+    const superStrongPasswordValidator = createValidator([
+    createLenghtValidator(config.MIN_ADMIN_PASSWORD_LEN),
+    validateContainsDigit,
+    validateContainsLowerCaseLetters,
+    validateContainsUpperCaseLetters
+    ], config.MINIMAL_VALIDATION_NUMBER); 
+  
 
  export { 
   validateMinimalLength, validateContainsDigit,
